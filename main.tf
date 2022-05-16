6dfb10b3f4d8203a6449e5e8b28453ef12f40474
acemk <ace@sonete.com>
2022-05-16 19:59:04 +0200

    Modify how subnet_mapping is obtained

--- main.tf
+++ main.tf
@@ -166,11 +166,11 @@ resource "aws_networkfirewall_firewall" "main" {
   firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
   vpc_id              = var.vpc_id
 
-  dynamic "subnet_mapping" {
-    for_each = var.subnet_mapping
-    content {
-      subnet_id = subnet_mapping.value
-    }
+  for_each = toset(var.subnet_mapping)
+  subnet_mapping {
+    subnet_id = each.key
   }
+
+
   tags = merge(var.tags)
 }
